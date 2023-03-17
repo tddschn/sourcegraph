79a55e060482d056921399b11908e932a5d68091
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2023-03-17 12:53:15 +0200

    repo-updater: rely on error returned from licensing.Check
    If prFeature.Check is unlicensed the error returned is the same error
    returned by the old final return NewFeatureNotActivatedError. This
    avoids that duplication and makes the function flatter.
    
    Test Plan: go test

--- enterprise/cmd/repo-updater/shared/licensing.go
+++ enterprise/cmd/repo-updater/shared/licensing.go
@@ -18,24 +18,25 @@ func enterpriseCreateRepoHook(ctx context.Context, s repos.Store, repo *types.Re
 		return nil
 	}
 
-	if prFeature := (&licensing.FeaturePrivateRepositories{}); licensing.Check(prFeature) == nil {
-		if prFeature.Unrestricted {
-			return nil
-		}
+	var prFeature licensing.FeaturePrivateRepositories
+	if err := licensing.Check(&prFeature); err != nil {
+		return err
+	}
 
-		numPrivateRepos, err := s.RepoStore().Count(ctx, ossDB.ReposListOptions{OnlyPrivate: true})
-		if err != nil {
-			return err
-		}
-
-		if numPrivateRepos >= prFeature.MaxNumPrivateRepos {
-			return errors.Newf("maximum number of private repositories included in license (%d) reached", prFeature.MaxNumPrivateRepos)
-		}
-
+	if prFeature.Unrestricted {
 		return nil
 	}
 
-	return licensing.NewFeatureNotActivatedError("The private repositories feature is not activated for this license. Please upgrade your license to use this feature.")
+	numPrivateRepos, err := s.RepoStore().Count(ctx, ossDB.ReposListOptions{OnlyPrivate: true})
+	if err != nil {
+		return err
+	}
+
+	if numPrivateRepos >= prFeature.MaxNumPrivateRepos {
+		return errors.Newf("maximum number of private repositories included in license (%d) reached", prFeature.MaxNumPrivateRepos)
+	}
+
+	return nil
 }
 
 // enterpriseUpdateRepoHook checks if there is still room for private repositories
@@ -47,28 +48,29 @@ func enterpriseUpdateRepoHook(ctx context.Context, s repos.Store, existingRepo *
 		return nil
 	}
 
-	if prFeature := (&licensing.FeaturePrivateRepositories{}); licensing.Check(prFeature) == nil {
-		if prFeature.Unrestricted {
-			return nil
-		}
+	var prFeature licensing.FeaturePrivateRepositories
+	if err := licensing.Check(&prFeature); err != nil {
+		return err
+	}
 
-		numPrivateRepos, err := s.RepoStore().Count(ctx, ossDB.ReposListOptions{OnlyPrivate: true})
-		if err != nil {
-			return err
-		}
-
-		if numPrivateRepos > prFeature.MaxNumPrivateRepos {
-			return errors.Newf("maximum number of private repositories included in license (%d) reached", prFeature.MaxNumPrivateRepos)
-		} else if numPrivateRepos == prFeature.MaxNumPrivateRepos {
-			// If the repository is already private, we don't have to check anything
-			newPrivateRepo := (!existingRepo.DeletedAt.IsZero() || !existingRepo.Private) && newRepo.Private // If restoring a deleted repository, or if it was a public repository, and is now private
-			if newPrivateRepo {
-				return errors.Newf("maximum number of private repositories included in license (%d) reached", prFeature.MaxNumPrivateRepos)
-			}
-		}
-
+	if prFeature.Unrestricted {
 		return nil
 	}
 
-	return licensing.NewFeatureNotActivatedError("The private repositories feature is not activated for this license. Please upgrade your license to use this feature.")
+	numPrivateRepos, err := s.RepoStore().Count(ctx, ossDB.ReposListOptions{OnlyPrivate: true})
+	if err != nil {
+		return err
+	}
+
+	if numPrivateRepos > prFeature.MaxNumPrivateRepos {
+		return errors.Newf("maximum number of private repositories included in license (%d) reached", prFeature.MaxNumPrivateRepos)
+	} else if numPrivateRepos == prFeature.MaxNumPrivateRepos {
+		// If the repository is already private, we don't have to check anything
+		newPrivateRepo := (!existingRepo.DeletedAt.IsZero() || !existingRepo.Private) && newRepo.Private // If restoring a deleted repository, or if it was a public repository, and is now private
+		if newPrivateRepo {
+			return errors.Newf("maximum number of private repositories included in license (%d) reached", prFeature.MaxNumPrivateRepos)
+		}
+	}
+
+	return nil
 }
