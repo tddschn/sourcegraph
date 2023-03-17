829092b6115d12c9f609b48c2bc86add57232fa7
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2023-03-17 16:17:12 +0200

    wip

--- enterprise/cmd/repo-updater/shared/licensing.go
+++ enterprise/cmd/repo-updater/shared/licensing.go
@@ -5,14 +5,36 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/licensing"
 	ossDB "github.com/sourcegraph/sourcegraph/internal/database"
+	"github.com/sourcegraph/sourcegraph/internal/extsvc"
 	"github.com/sourcegraph/sourcegraph/internal/repos"
 	"github.com/sourcegraph/sourcegraph/internal/types"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
-// enterpriseCreateRepoHook checks if there is still room for private repositories
-// available in the applied license before creating a new private repository.
+// enterpriseCreateRepoHook enforces licences checks which may prevent a repository from being created.
 func enterpriseCreateRepoHook(ctx context.Context, s repos.Store, repo *types.Repo) error {
+	if err := enterpriseCreateRepoHook_FeaturePrivateRepositories(ctx, s, repo); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// enterpriseUpdateRepoHook checks if there is still room for private repositories
+// available in the applied license before updating a repository from public to private,
+// or undeleting a private repository.
+func enterpriseUpdateRepoHook(ctx context.Context, s repos.Store, existingRepo *types.Repo, newRepo *types.Repo) error {
+	if err := enterpriseUpdateRepoHook_FeaturePrivateRepositories(ctx, s, existingRepo, newRepo); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// enterpriseCreateRepoHook_FeaturePrivateRepositories checks if there is
+// still room for private repositories available in the applied license before
+// creating a new private repository.
+func enterpriseCreateRepoHook_FeaturePrivateRepositories(ctx context.Context, s repos.Store, repo *types.Repo) error {
 	// If the repository is public, we don't have to check anything
 	if !repo.Private {
 		return nil
@@ -39,10 +61,11 @@ func enterpriseCreateRepoHook(ctx context.Context, s repos.Store, repo *types.Re
 	return nil
 }
 
-// enterpriseUpdateRepoHook checks if there is still room for private repositories
-// available in the applied license before updating a repository from public to private,
-// or undeleting a private repository.
-func enterpriseUpdateRepoHook(ctx context.Context, s repos.Store, existingRepo *types.Repo, newRepo *types.Repo) error {
+// enterpriseUpdateRepoHook_FeaturePrivateRepositories checks if there is
+// still room for private repositories available in the applied license before
+// updating a repository from public to private, or undeleting a private
+// repository.
+func enterpriseUpdateRepoHook_FeaturePrivateRepositories(ctx context.Context, s repos.Store, existingRepo *types.Repo, newRepo *types.Repo) error {
 	// If it is being updated to a public repository, or if a repository is being deleted, we don't have to check anything
 	if !newRepo.Private || !newRepo.DeletedAt.IsZero() {
 		return nil
@@ -70,6 +93,39 @@ func enterpriseUpdateRepoHook(ctx context.Context, s repos.Store, existingRepo *
 		if newPrivateRepo {
 			return errors.Newf("maximum number of private repositories included in license (%d) reached", prFeature.MaxNumPrivateRepos)
 		}
+	}
+
+	return nil
+}
+
+// TODO enterpriseCreateRepoHook_FeaturePrivateRepositories checks if there is
+// still room for private repositories available in the applied license before
+// creating a new private repository.
+func enterpriseCreateRepoHook_FeatureRemoteRepositories(ctx context.Context, s repos.Store, repo *types.Repo) error {
+	// If the repository is not remote it doesn't count towards remote limits.
+	// TODO more robust check living somewhere that makes sense
+	if repo.ExternalRepo.ServiceType == extsvc.TypeOther {
+		return nil
+	}
+
+	var feat licensing.FeatureRemoteRepositories
+	if err := licensing.Check(&feat); err != nil {
+		return err
+	}
+
+	if feat.Unrestricted {
+		return nil
+	}
+
+	// TODO need to add a robust check here. Thinking of a way to list "remote" other external services?
+
+	numRemoteRepos, err := s.RepoStore().Count(ctx, ossDB.ReposListOptions{OnlyPrivate: true})
+	if err != nil {
+		return err
+	}
+
+	if numPrivateRepos >= prFeature.MaxNumPrivateRepos {
+		return errors.Newf("maximum number of private repositories included in license (%d) reached", prFeature.MaxNumPrivateRepos)
 	}
 
 	return nil
